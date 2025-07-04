a229c02bc8d15cb92aa2f9a20182655b7c2a5e31
amani <bataganiramani@gmail.com>
2025-07-04 19:04:40 +0200

    video + crop done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,43 @@
 export default function Home() {
   return (
     <>
-      <div className="MainDiv">
-        <video
-          src="/videos/Attack on Titan - S2E12 - 37 (108)..mp4"
-          width={640}
-          height={360}
-          autoPlay
-          loop
-          muted
-          playsInline
-          style={{ display: "block", margin: "0 auto" }}
+      
+
+      <div className="MainDiv" >
+        <div className="heroText">
+          <h1>
+            Every single animes you ever needed, in ONE place
+          </h1>
+          <button className="btn">Explore</button>
+        </div>
+        <div className="video-crop-container">
+          <video
+            className="video-crop"
+            src="/videos/One Punch Man - S2E4 (57).mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
           />
+
+          <video
+            className="video-crop"
+            src="/videos/Vinland Saga - S1E23 (6).mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+
+          <video
+            className="video-crop"
+            src="/videos/Demon Slayer - S3E11 (38).mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+        </div>
       </div>
     </>
   );
