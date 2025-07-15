cc874d785ef955427faa721004523833f1b3b42f
amani <bataganiramani@gmail.com>
2025-07-15 16:09:55 +0200

    style adjustement

--- src/component/sidenav/SideNav.jsx
+++ src/component/sidenav/SideNav.jsx
@@ -1,19 +1,39 @@
+'use client'
 import Link from 'next/link'
 import style from './sideNav.module.css'
+import { usePathname } from 'next/navigation'
 
 export default function SideNav() {
+    const pathname = usePathname();
+
+    const isActive = (path) => {
+        // Pour la page d'accueil
+        if (path === '/' && pathname === '/') return true;
+        // Pour les autres pages, vérifier si le chemin actuel commence par le chemin du lien
+        return path !== '/' && pathname.startsWith(path);
+    };
     
     return(
-        <>
-            <div className={style.SideNav}>
-                <ul className={style.NavIcon}>
-                    <Link href='/' className={style.icon}><i className="fa-solid fa-house"></i></Link>
-                    <Link href='/features/discover' className={style.icon}><i className="fa-solid fa-compass"></i></Link>
-                    <Link href='/features/collection' className={style.icon}><i className="fa-regular fa-star"></i></Link>
-                </ul>
-                
-            </div>
-        </>
+        <nav className={style.SideNav}>
+            <ul className={style.NavIcon}>
+                <Link href='/' className={isActive('/') ? style.activeIcon : style.icon}>
+                    <i className="fa-solid fa-house"></i>
+                    <span className={style.iconLabel}>Home</span>
+                </Link>
+                <Link href='/features/discover' className={isActive('/features/discover') ? style.activeIcon : style.icon}>
+                    <i className="fa-solid fa-compass"></i>
+                    <span className={style.iconLabel}>Discover</span>
+                </Link>
+                <Link href='/features/collection' className={isActive('/features/collection') ? style.activeIcon : style.icon}>
+                    <i className="fa-regular fa-star"></i>
+                    <span className={style.iconLabel}>Collection</span>
+                </Link>
+                <Link href='/features/myaccount' className={isActive('/features/myaccount') ? style.activeIcon : style.icon}>
+                    <i className="fa-solid fa-user"></i>
+                    <span className={style.iconLabel}>Account</span>
+                </Link>
+            </ul>
+        </nav>
     )
 }
 
